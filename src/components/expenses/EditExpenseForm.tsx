4720fdcc972e0a89b6e3bae1cc577708dfc60514
Jonatas Henrique <jonatas.reisprofissional@gmail.com>
2025-09-26 14:06:48 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/components/expenses/EditExpenseForm.tsx
+++ src/components/expenses/EditExpenseForm.tsx
@@ -64,7 +64,7 @@ export function EditExpenseForm({ expense }: { expense: Expense }) {
       createdBy: expense.createdBy,
     },
   });
-
+  
   useEffect(() => {
     async function fetchExpenseTypes() {
         try {
